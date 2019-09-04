25e80309f944dab64cd3f319a12de9592e3f9b9f
Sugydro <boobs@spaces.ru>
2019-09-04 15:27:44 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,35 @@
 <?php
-header('X-PHP: test');
-http_response_code($_GET['code'] ?? 200);
-echo 'Test output';
-var_dump($_GET);
-var_dump(opcache_get_status());
+
+require_once 'archillect.class.php';
+
+if(!empty($_GET['method']))
+{
+    switch (strtolower($_GET['method']))
+    {
+        case 'getlastid':
+            echo json_encode(Archillect::GetLastImageID());
+            break;
+        case 'getlast':
+            if(isset($_GET['amount']) && is_numeric($_GET['amount'])) {
+                echo json_encode(Archillect::GetLast($_GET['amount']));
+            }
+            else
+                echo 'Invalid amount';
+            break;
+        case 'getbyid':
+            if(isset($_GET['id']) && is_numeric($_GET['id'])) {
+                echo json_encode(Archillect::GetImageData($_GET['id']));
+            }
+            else
+                echo 'Invalid id';
+            break;
+        case 'getrandom':
+            echo json_encode(Archillect::GetRandom());
+            break;
+        default:
+            echo 'Unknown method';
+            break;
+    }
+}
+else
+    echo 'Method not selected';
