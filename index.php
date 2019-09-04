f8d21f4b63c6759b169cb6bb5e517272ce14ae5a
Sugydro <boobs@spaces.ru>
2019-09-04 13:43:30 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,8 @@
 <?php
 
-require_once 'archillect.class.php';
-
-if(!empty($_GET['method']))
-{
-    switch (strtolower($_GET['method']))
-    {
-        case 'getlastid':
-            echo json_encode(Archillect::GetLastImageID());
-            break;
-        case 'getlast':
-            if(isset($_GET['amount']) && is_numeric($_GET['amount'])) {
-                echo json_encode(Archillect::GetLast($_GET['amount']));
-            }
-            else
-                echo 'Invalid amount';
-            break;
-        case 'getbyid':
-            if(isset($_GET['id']) && is_numeric($_GET['id'])) {
-                echo json_encode(Archillect::GetImageData($_GET['id']));
-            }
-            else
-                echo 'Invalid id';
-            break;
-        case 'getrandom':
-            echo json_encode(Archillect::GetRandom());
-            break;
-        default:
-            echo 'Unknown method';
-            break;
-    }
-}
-else
-    echo 'Method not selected';
+var_dump(file_get_contents('php://input'));
+var_dump($_GET);
+var_dump($_POST);
+var_dump($_SERVER);
+var_dump($_ENV);
+var_dump($_FILES);
